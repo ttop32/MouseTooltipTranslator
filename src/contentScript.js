720ffc9cda7e11a40cba02ce6b673caf1130f67c
ttop324 <ttop324@gmail.com>
2021-01-03 10:47:40 +0000

    0.1.2
    0.1.2

--- src/contentScript.js
+++ src/contentScript.js
@@ -10,28 +10,26 @@ import $ from "jquery";
 require('popper.js');
 require('bootstrap');
 
+
+
+
+
 var clientX = 0;
 var clientY = 0;
 var clientXScroll = 0;
 var clientYScroll = 0;
-var clientTarget = null;
 var activatedWord = null;
 var keyDownList = { //use key down for enable translation partially
   17: false, //ctrl
   16: false, //shift
   18: false //alt
 };
-
-
-
-
 //use mouse position for tooltip position
 $(document).mousemove(function(event) {
   clientX = event.clientX;
   clientY = event.clientY;
   clientXScroll = event.pageX;
   clientYScroll = event.pageY;
-  clientTarget = event.target;
   setTooltipPosition();
 });
 document.addEventListener("visibilitychange", function() { //detect tab swtich to turn off key down
@@ -63,7 +61,7 @@ $(document).keyup(function(e) {
 //tooltip: init
 $(document).ready(function() {
   $('<div/>', {
-    id: 'bubble',
+    id: 'mttContainer',
     class: 'bootstrapiso',
     css: {
       "position": "absolute",
@@ -74,47 +72,47 @@ $(document).ready(function() {
     }
   }).appendTo(document.body);
 
-  $('#bubble').tooltip({
+  $('#mttContainer').tooltip({
     placement: "top",
-    container: "#bubble",
+    container: "#mttContainer",
     animation: false
   });
 });
 
 //tooltip: word detection, show & hide
 setInterval(function() {
-  var word = getWordUnderMouse(clientX, clientY, clientTarget);
+  var word = getMouseOverWord(clientX, clientY);
 
   if (word != null && word.length != 0 && activatedWord != word) { //show tooltip, if current word is changed and word is not none
     translateSentence(word, "auto", "ko", function(translatedSentence, lang) {
       if (translatedSentence.length != 0) { // only show tooltip when word lang is not user lang
-        $('#bubble').attr('data-original-title', translatedSentence);
+        $('#mttContainer').attr('data-original-title', translatedSentence);
         tts(word, lang);
       } else {
-        $('#bubble').attr('data-original-title', "");
-        $('#bubble').tooltip("hide");
+        $('#mttContainer').attr('data-original-title', "");
+        $('#mttContainer').tooltip("hide");
       }
       activatedWord = word;
     });
   } else if ((word == null || word.length == 0) && activatedWord != null) { //hide tooltip, if activated word exist and current word is none
     activatedWord = null;
-    $('#bubble').attr('data-original-title', "");
-    $('#bubble').tooltip("hide");
+    $('#mttContainer').attr('data-original-title', "");
+    $('#mttContainer').tooltip("hide");
   }
   setTooltipPosition(); //tooltip movement
 }, 700);
 
 function setTooltipPosition() {
   if (activatedWord != null) {
-    $('#bubble').css('left', clientXScroll).css('top', clientYScroll);
-    $("#bubble").tooltip("show");
+    $('#mttContainer').css('left', clientXScroll).css('top', clientYScroll);
+    $("#mttContainer").tooltip("show");
   }
 }
 
-function getWordUnderMouse(clientX, clientY, target) {
+function getMouseOverWord(clientX, clientY) {
   var range = document.caretRangeFromPoint(clientX, clientY);
-  //no mouse target,no range,is not text
-  if (target == null || range == null || range.startContainer == null || range.startContainer.nodeType !== Node.TEXT_NODE)
+  //if no range or is not text, give null
+  if (range == null || range.startContainer == null || range.startContainer.nodeType !== Node.TEXT_NODE)
     return null;
   range.expand('sentence'); //range.expand('word');
   var rect = range.getBoundingClientRect(); //mouse in word rect
@@ -153,3 +151,7 @@ function tts(word, lang) {
     function(data) {}
   );
 }
+
+//support pdf tooltip translation using Mozilla PDF.js
+//increase tooltip margin
+//prevent translation on url text
