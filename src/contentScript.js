e263b68fa89d3d8273811dab56ba31d20c571e01
san <san@wisebits.com>
2021-09-05 14:49:32 +0300

    Translate on selection

--- src/contentScript.js
+++ src/contentScript.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
 enableSelectionEndEvent();
 
 document.addEventListener("selectionEnd", async function(event) {
-  // if translate on selection enabled
+  // if translate on selection is enabled
   if (document.visibilityState === "visible" && settingLoaded && currentSetting["translateOnSelection"] === "true") {
     selectedText = event.selectedText;
     await processWord(selectedText);
@@ -185,8 +185,8 @@ function getMouseOverWord(clientX, clientY) {
 }
 
 function filterWord(word) {
-  word = word.trim(); // remove whitespaces from begin and end
   word = word.replace(/\s+/g, ' ').trim(); //replace whitespace as single space
+  word = word.trim(); // remove whitespaces from begin and end of word
   if (word.length > 1000 || //filter out text that has over 1000length
     isUrl(word) || //if it is url
     !/[^\s\d»«…~`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/g.test(word)) { // filter one that only include num,space and special char(include currency sign) as combination
