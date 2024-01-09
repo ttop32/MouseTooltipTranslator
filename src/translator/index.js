a42f11adfeeafe604c0eaf8280b314db3e09fdfc
daniel k <ttop324@gmail.com>
2024-01-10 00:19:39 +0900

    add baidu

--- src/translator/index.js
+++ src/translator/index.js
@@ -3,5 +3,6 @@ import bing from "./bing.js";
 import papago from "./papago.js";
 import deepl from "./deepl.js";
 import yandex from "./yandex.js";
+import baidu from "./baidu.js";
 
-export default { google, bing, papago, deepl, yandex };
+export default { google, bing, papago, deepl, yandex, baidu };
