4d1931278bf2fe199a8278dcaa2d7fac8dddc338
daniel k <ttop324@gmail.com>
2024-02-07 20:24:51 +0900

    refactor chatgpt code

--- src/translator/chatgpt.js
+++ src/translator/chatgpt.js
@@ -5,40 +5,58 @@ import { v4 as uuidv4 } from "uuid";
 var authUrl = "https://chat.openai.com/api/auth/session";
 var modelUrl = "https://chat.openai.com/backend-api/models";
 var conversationUrl = "https://chat.openai.com/backend-api/conversation";
+var parent_message_id = uuidv4();
+let abortController = new AbortController();
+
+var token;
+var accessTokenExpires;
+//https://aletheia.medium.com/demystifing-chatgpt-c9ab468ccc84
+// https://getstream.io/blog/build-a-chat-app-with-openai-chatgpt/
 
 export default class chatgpt extends BaseTranslator {
   static async requestTranslate(text, sourceLang, targetLang) {
-    var { accessToken, expires } = await getToken();
+    // abortController.abort();
+    // abortController = new AbortController();
+    var chatToken = await getToken();
+    var text = "Who are you?";
 
-    var parentMessageId = uuidv4();
-    var id = uuidv4();
-
-    console.log(accessToken, parentMessageId, id);
-    return await ky.post(conversationUrl, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-        Accept: " text/event-stream",
-      },
-      body: JSON.stringify({
-        action: "next",
-        messages: [
-          {
-            id,
-            role: "user",
-            content: { content_type: "text", parts: ["Who are you?"] },
-          },
-        ],
-        conversation_id: null,
-        parentMessageId,
-        model: "text-davinci-002-render",
-      }),
-    });
+    return await ky
+      .post(conversationUrl, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${chatToken}`,
+          Accept: " text/event-stream",
+        },
+        body: JSON.stringify({
+          action: "next",
+          messages: [
+            {
+              id: uuidv4(),
+              role: "user",
+              content: { content_type: "text", parts: [text] },
+            },
+          ],
+          conversation_id: undefined,
+          parent_message_id,
+          model: "text-davinci-002-render",
+        }),
+        // signal: abortController.signal,
+      })
+      .text();
   }
   static async wrapResponse(res, text, sourceLang, targetLang) {
-    console.log(res);
+    const dataChunks = res.split("data:");
+    const responseObjectText = dataChunks[dataChunks.length - 2].trim();
+    const responseObject = JSON.parse(responseObjectText);
+    var translatedText = responseObject.message.content.parts.join(" ");
+    var conversation_id = responseObject.conversation_id;
+    await deleteConversation(conversation_id);
+
+    console.log(responseObject);
+    console.log(conversation_id);
+
     return {
-      translatedText: "",
+      translatedText,
       detectedLang: "",
       transliteration: "",
     };
@@ -46,6 +64,27 @@ export default class chatgpt extends BaseTranslator {
 }
 
 async function getToken() {
-  var res = await ky.get(authUrl).json();
-  return res;
+  if (Date.parse(accessTokenExpires) > Date.now()) {
+    return token;
+  }
+  var { accessToken, expires } = await ky.get(authUrl).json();
+  token = accessToken;
+  accessTokenExpires = expires;
+  return token;
+}
+
+async function deleteConversation(conversation_id) {
+  const chatToken = await getToken();
+  return await ky
+    .patch(
+      `https://chat.openai.com/backend-api/conversation/${conversation_id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${chatToken}`,
+        },
+        body: JSON.stringify({ is_visible: false }),
+      }
+    )
+    .json();
 }
