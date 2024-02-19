b7f2927c0833fd9308ebd368eaa778b623146014
daniel k <ttop324@gmail.com>
2024-02-20 01:11:21 +0900

    add lingva

---
+++ src/translator/lingva.js
@@ -0,0 +1,20 @@
+import BaseTranslator from "./baseTranslator";
+
+import ky from "ky";
+
+const apiPath = "https://lingva.ml/api/v1/";
+
+export default class lingva extends BaseTranslator {
+  static async requestTranslate(text, sourceLang, targetLang) {
+    return await ky(
+      `${apiPath}${sourceLang}/${targetLang}/${encodeURIComponent(text)}`
+    ).json();
+  }
+  static async wrapResponse(res, text, sourceLang, targetLang) {
+    return {
+      translatedText: res["translation"],
+      detectedLang: res["info"]["translation"],
+      transliteration: res["info"]["pronunciation"]["query"],
+    };
+  }
+}
