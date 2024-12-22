d33ce6e5b4d82bb63fa0bd0f447a477cb9b2d069
ttop32 <ttop324@gmail.com>
2024-12-22 19:06:21 +0900

    add sub shadow

--- src/contentScript.js
+++ src/contentScript.js
@@ -890,7 +890,7 @@ function applyStyleSetting() {
     }
     .ytp-caption-segment{
       color: white !important;
-      text-shadow: 1px 1px 2px black !important;
+      text-shadow: rgba(255, 255, 255) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0) 0px 4px 6px, rgba(0, 0, 0) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
       background:transparent  !important;
     }
     .captions-text:hover .caption-visual-line:first-of-type:after {
